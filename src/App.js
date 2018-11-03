f5dd843f11fc1c8f585b4d5fad6ef10896e600e8
wj-soft <wooju_lee@daum.net>
2018-11-03 17:28:18 +0900

    meterial ui 프레임워크 추가 및 테스트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         HelloWorld
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </div>
     );
   }
