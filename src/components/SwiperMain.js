898e9cea9464d68851f7ee69c098428857a88def
nara0974 <naas0974@gmail.com>
2019-01-05 12:15:29 +0900

    SwiperMain 스와이퍼 기능 수정 및 index.css 스와이퍼 img 크기 수정

--- src/components/SwiperMain.js
+++ src/components/SwiperMain.js
@@ -18,25 +18,27 @@ class SwiperMain extends Component {
 
   render(){
     const params = {
+      slidesPerView: 1,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
       navigation: {
         nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+        prevEl: '.swiper-button-prev',
       }
-    }
+    };
 
     return (
         <div>
-          <Swiper {...params} shouldSwiperUpdate="true" >
-             
+          <Swiper {...params} shouldSwiperUpdate="true">
               {!this.props.FindRes.addImg ? false : this.props.FindRes.addImg.map((v, i) => {
-                return  <div><img src={v.originimgurl} alt={this.props.FindRes.contentInfo.title} key={i}/></div>
+                return  <div shouldSwiperUpdate="true" ><img src={v.originimgurl} alt={this.props.FindRes.contentInfo.title} key={i}/></div>
               })}
             
-            
-              {!this.props.FindRes.menuImg ? false : this.props.FindRes.menuImg.map((v, i) => {
+              { !this.props.FindRes.menuImg ? false : this.props.FindRes.menuImg.map((v, i) => {
                 return  <div><img src={v.originimgurl} alt={this.props.FindRes.contentInfo.title} key={i}/></div>
-              })}
-            
+              })} 
           </Swiper>
         </div>
     );
