a2369527fe0d9041ed4a3baf2afc956bb0b0e9c8
wj-soft <wooju_lee@daum.net>
2019-02-04 19:09:41 +0900

    썸네일 이미지 높이값 고정

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,20 @@ import { Link } from 'react-router-dom';
 class Card extends Component {
   render() {
     const imgUrl = this.props.imgUrl ? this.props.imgUrl : 'http://kaverisias.com/wp-content/uploads/2018/01/catalog-default-img.gif'
+    
+    let styles = {
+      height: '500px',
+      backgroundColor: 'yellow',
+    };
+
+
     return (
       <li className="col">
           {/* <a href="#"></a> */}
           <div className="card">
             <div className="content-img">
               <Link to={`/detail/${this.props.resId}`}>
-                <img src={imgUrl} alt="" />
+              <img src={imgUrl} alt="" style={styles}/>
               </Link>
             </div>
             <div className="content-txt">
