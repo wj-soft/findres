d145e648386ec41cea5f19a877cc24beff2e4a36
wj-soft <wooju_lee@daum.net>
2019-02-04 19:27:18 +0900

    jsx 일부 수정

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,19 +14,18 @@ class Card extends Component {
     return (
       <li className="col">
           {/* <a href="#"></a> */}
-          <div className="card">
-            <div className="content-img">
-              <Link to={`/detail/${this.props.resId}`}>
-              <img src={imgUrl} alt="" style={styles}/>
-              </Link>
-            </div>
-            <div className="content-txt">
-                <h3 className="tit">
-                  <Link to={`/detail/${this.props.resId}`}>{this.props.title}</Link></h3>
-                <p className="desc">{this.props.addr}</p>
-              </div>
-            </div>
-        </li>
+        <div className="card">
+          <div className="content-img">
+            <Link to={`/detail/${this.props.resId}`}>
+            <img src={imgUrl} alt="" style={styles}/>
+            </Link>
+          </div>
+          <div className="content-txt">
+            <h3 className="tit"><Link to={`/detail/${this.props.resId}`}>{this.props.title ? this.props.title : 'noTitle'}</Link></h3>
+            <p className="desc">{this.props.addr ? this.props.addr : '--'}</p>
+          </div>
+        </div>
+      </li>
     );
   }
 }
